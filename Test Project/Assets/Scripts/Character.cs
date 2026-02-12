df10290702c71d00a8100b3467e5867b03b7cb77
Summer Smith <summersmith2676@gmail.com>
2026-02-12 11:16:36 -0500

    Character class :-)

--- Test Project/Assets/Scripts/Character.cs
+++ Test Project/Assets/Scripts/Character.cs
@@ -4,15 +4,128 @@ using UnityEngine;
 
 public class Character : DungeonMaster
 {
+    public bool diceAveraged;
+    public string characterName;
+    public int characterLevel;
+    public int conScore;
+    public string characterRace;
+    public string characterClass;
+    public bool toughFeat;
+    public bool stoutFeat;
+    private string rollType;
+    private int totalHP;
+
+    public Character(string name, int level, int con, string race, string cclass, bool tfeat, bool sfeat)
+    {
+        this.characterName = name;
+        this.characterLevel = level;
+        this.characterClass = cclass;
+        this.conScore = con;
+        this.characterRace = race;
+        this.toughFeat = tfeat;
+        this.stoutFeat = sfeat;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Character character = new Character("Test Character", 1, 10, "Dwarf", "Fighter", true, true);
+        Instantiate(character);
+            totalHP = calculateHP();
+            output(character);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public int calculateHP()
+    {
+        int hitPoints;
+
+        if (diceAveraged)
+        {
+            rollType = "averaged";
+            int averagedRoll; // This is turned into an int so that it can be rounded up.
+            switch (characterClasses[characterClass])
+            {
+                case 6:
+                    averagedRoll = 4;
+                    break;
+                case 8:
+                    averagedRoll = 5;
+                    break;
+                case 10:
+                    averagedRoll = 6;
+                    break;
+                case 12:
+                    averagedRoll = 7;
+                    break;
+                default: // This shouldn't be possible, but it has been added in for the sake of debugging.
+                    averagedRoll = 0;
+                    break;
+            }
+
+            if (toughFeat && stoutFeat)
+            {
+                hitPoints = (characterLevel * averagedRoll) + (characterLevel * conScores[conScore]) +
+                            (characterLevel * characterRaces[characterRace]) + (characterLevel * 3);
+            }
+            else if (toughFeat)
+            {
+                hitPoints = (characterLevel * averagedRoll) + (characterLevel * conScores[conScore]) +
+                            (characterLevel * characterRaces[characterRace]) + (characterLevel * 2);
+            }
+            else if (stoutFeat)
+            {
+                hitPoints = (characterLevel * averagedRoll) + (characterLevel * conScores[conScore]) +
+                            (characterLevel * characterRaces[characterRace]) + (characterLevel * 1);
+            }
+            else
+            {
+                hitPoints = (characterLevel * averagedRoll) + (characterLevel * conScores[conScore]) +
+                            (characterLevel * characterRaces[characterRace]);
+            }
+        }
+        else
+        {
+            rollType = "rolled";
+            int randomRoll = Random.Range(1, characterClasses[characterClass] + 1);
+            hitPoints = (characterLevel * randomRoll) + (characterLevel * conScores[conScore]) + (characterLevel * characterRaces[characterRace]);
+        }
+
+        return hitPoints;
+    }
+
+    public void output(Character character)
+    {
+        if (toughFeat && stoutFeat)
+        {
+            Debug.LogFormat("My character " + characterName + " is a level " + characterLevel + " " + characterClass +
+                            " with a CON score of " + conScore + " and is of the " + characterRace +
+                            " race and has the Tough and Stout feats. I want the HP " + rollType + ".");
+        }
+        else if (toughFeat)
+        {
+            Debug.Log("My character " + characterName + " is a level " + characterLevel + " " + characterClass +
+                      " with a CON score of " + conScore + " and is of the " + characterRace +
+                      " race and has the Tough feat. I want the HP " + rollType + ".");
+        }
+        else if (stoutFeat)
+        {
+            Debug.Log("My character " + characterName + " is a level " + characterLevel + " " + characterClass +
+                      " with a CON score of " + conScore + " and is of the " + characterRace +
+                      " race and has the Stout feat. I want the HP " + rollType + ".");
+        }
+        else
+        {
+            Debug.Log("My character " + characterName + " is a level " + characterLevel + " " + characterClass +
+                      " with a CON score of " + conScore + " and is of the " + characterRace +
+                      " race and has no feats. I want the HP " + rollType + ".");
+        }
+        Debug.Log("Total HP: " + totalHP);
     }
 }
